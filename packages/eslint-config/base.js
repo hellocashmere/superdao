c1c7e2a1895988d8ed5e8fda8bca6cfaaef7a73f
hellocashmere <isasdf@yandex.ru>
2026-08-29 21:39:17 +0300

    [config] Update ESLint configuration

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,9 +1,20 @@
-import js from "@eslint/js"
-import eslintConfigPrettier from "eslint-config-prettier"
-import importPlugin from "eslint-plugin-import"
-import onlyWarn from "eslint-plugin-only-warn"
-import turboPlugin from "eslint-plugin-turbo"
-import tseslint from "typescript-eslint"
+import js from "@eslint/js";
+import eslintConfigPrettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
+import onlyWarn from "eslint-plugin-only-warn";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import turboPlugin from "eslint-plugin-turbo";
+import tseslint from "typescript-eslint";
+
+const importSortGroups = [
+  ["^node:"],
+  ["^react", "^next"],
+  ["^@?\\w"],
+  ["^@/"],
+  ["^\\u0000"],
+  ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+  ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+];
 
 /**
  * A shared ESLint configuration for the repository.
@@ -17,6 +28,7 @@ export const config = [
   {
     plugins: {
       import: importPlugin,
+      "simple-import-sort": simpleImportSort,
       turbo: turboPlugin,
     },
     rules: {
@@ -28,6 +40,9 @@ export const config = [
         },
       ],
       "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+      "quote-props": ["error", "as-needed"],
+      "simple-import-sort/imports": ["error", { groups: importSortGroups }],
+      "simple-import-sort/exports": "error",
       "@typescript-eslint/no-empty-object-type": [
         "error",
         { allowInterfaces: "with-single-extends" },
@@ -43,4 +58,4 @@ export const config = [
   {
     ignores: ["dist/**", ".next/**", "**/.turbo/**", "**/coverage/**"],
   },
-]
+];
