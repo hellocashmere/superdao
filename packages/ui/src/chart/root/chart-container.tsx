a5300272950c55cb2453abdebaed3620dae8e7dc
hellocashmere <isasdf@yandex.ru>
2026-09-02 20:52:20 +0300

    [ui][chart] Standardize chart identifier naming

--- packages/ui/src/chart/root/chart-container.tsx
+++ packages/ui/src/chart/root/chart-container.tsx
@@ -55,14 +55,14 @@ export function ChartContainer({
   initialDimension = INITIAL_DIMENSION,
   ...props
 }: ChartContainerProps) {
-  const uniqueId = useId();
-  const chartId = `chart-${id ?? uniqueId.replace(/:/g, "")}`;
+  const uniqueID = useId();
+  const chartID = `chart-${id ?? uniqueID.replace(/:/g, "")}`;
 
   return (
     <ChartContext.Provider value={{ config }}>
       <div
         data-slot="chart"
-        data-chart={chartId}
+        data-chart={chartID}
         className={cn(
           "flex aspect-video justify-center text-xs [&_.recharts-cartesian-axis-tick_text]:fill-muted-foreground [&_.recharts-cartesian-grid_line[stroke='#ccc']]:stroke-border/50 [&_.recharts-curve.recharts-tooltip-cursor]:stroke-border [&_.recharts-dot[stroke='#fff']]:stroke-transparent [&_.recharts-layer]:outline-hidden [&_.recharts-polar-grid_[stroke='#ccc']]:stroke-border [&_.recharts-radial-bar-background-sector]:fill-muted [&_.recharts-rectangle.recharts-tooltip-cursor]:fill-muted [&_.recharts-reference-line_[stroke='#ccc']]:stroke-border [&_.recharts-sector]:outline-hidden [&_.recharts-sector[stroke='#fff']]:stroke-transparent [&_.recharts-surface]:outline-hidden",
           className
@@ -70,7 +70,7 @@ export function ChartContainer({
         {...props}
       >
         <ChartStyle
-          id={chartId}
+          id={chartID}
           config={config}
         />
         <RechartsPrimitive.ResponsiveContainer initialDimension={initialDimension}>
